ad652f15e97aa6943510781a8f6cb98dd55e1e51
Andy Gallagher <fredex42@gmail.com>
2022-07-26 09:48:40 +0100

    WIP building CDK to deploy cluster

---
+++ aws/cdk/lib/eks.ts
@@ -0,0 +1,84 @@
+import {Construct} from "constructs";
+import {GuStack} from "@guardian/cdk/lib/constructs/core";
+import {InstanceClass, InstanceSize, InstanceType, ISubnet, IVpc} from "aws-cdk-lib/aws-ec2";
+import * as eks from "aws-cdk-lib/aws-eks";
+import {CapacityType, DefaultCapacityType, NodegroupAmiType} from "aws-cdk-lib/aws-eks";
+import * as kms from "aws-cdk-lib/aws-kms";
+import {ManagedPolicy, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {Stack} from "aws-cdk-lib";
+
+interface ElasticPlaygroundClusterProps {
+    vpc: IVpc;
+    subnets: ISubnet[];
+    maxNodesPerAZ?: number;
+}
+
+const workerRolePolicies = [
+    "AmazonEKSWorkerNodePolicy",
+    "AmazonEC2ContainerRegistryReadOnly",
+    "AmazonSSMManagedInstanceCore",
+    "AmazonEKS_CNI_Policy",
+];
+
+//FIXME: needs to be filled in
+const controlPlaneRolePolicies = [
+    "AmazonEKSClusterPolicy"
+];
+
+export class ElasticPlaygroundCluster extends Construct {
+
+    constructor(scope: GuStack, id: string, props:ElasticPlaygroundClusterProps) {
+        super(scope, id);
+
+        const secretsKey = new kms.Key(this, "KubeSecretsKey", {
+
+        });
+
+        const clusterRole = new Role(this, "KubeControlRole", {
+            assumedBy: new ServicePrincipal('eks.amazonaws.com'),
+            managedPolicies: controlPlaneRolePolicies.map(n=>ManagedPolicy.fromAwsManagedPolicyName(n)),
+            description: "Role for EKS cluster control-plane nodes"
+        });
+
+        const workerRole = new Role(this, "KubeWorkerRole", {
+            assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
+            managedPolicies: workerRolePolicies.map(n=>ManagedPolicy.fromAwsManagedPolicyName(n)),
+            description: "Role for EKS cluster worker nodes"
+        });
+
+        const cluster = new eks.Cluster(this, "Cluster", {
+            role: clusterRole,
+            secretsEncryptionKey: secretsKey,
+            vpcSubnets: [
+                {
+                    subnets: props.subnets,
+                }
+            ],
+            version: eks.KubernetesVersion.V1_21,
+            vpc: props.vpc,
+            defaultCapacity: 0,
+        });
+        //FIXME: remember to add a service account linked to role that allows S3 snapshot bucket access
+        //this should come in a seperate submodule.
+
+
+        Stack.of(this).availabilityZones.forEach(zoneId=>
+            cluster.addNodegroupCapacity(`NodeGroup${zoneId}`, {
+                    subnets: {
+                        onePerAz: true,
+                        availabilityZones: [zoneId],
+                        subnets: props.subnets,
+                    },
+                    amiType: NodegroupAmiType.AL2_ARM_64,
+                    minSize: 0,
+                    maxSize: props.maxNodesPerAZ ?? 1,
+                    desiredSize: props.maxNodesPerAZ ? props.maxNodesPerAZ >=1 ? props.maxNodesPerAZ/2 : 1 : 1,
+                    instanceTypes: [
+                        InstanceType.of(InstanceClass.R6G, InstanceSize.LARGE),
+                    ],
+                    nodeRole: workerRole,
+                    capacityType: CapacityType.ON_DEMAND,
+                })
+        )
+    }
+}
--- aws/cdk/lib/elasticplayground.ts
+++ aws/cdk/lib/elasticplayground.ts
@@ -1,17 +1,23 @@
-import { join } from 'path';
-import type { GuStackProps } from '@guardian/cdk/lib/constructs/core';
-import { GuStack } from '@guardian/cdk/lib/constructs/core';
-import type { App } from 'aws-cdk-lib';
-import { CfnInclude } from 'aws-cdk-lib/cloudformation-include';
-import {GuEc2App} from "@guardian/cdk";
-import {GuVpc} from "@guardian/cdk/lib/constructs/ec2";
+import type {GuStackProps} from '@guardian/cdk/lib/constructs/core';
+import {GuStack} from '@guardian/cdk/lib/constructs/core';
+import type {App} from 'aws-cdk-lib';
+import {GuVpc, SubnetType} from "@guardian/cdk/lib/constructs/ec2";
+import {APP_NAME} from "./constants";
+import {ElasticPlaygroundCluster} from "./eks";
 
 export class Elasticplayground extends GuStack {
 	constructor(scope: App, id: string, props: GuStackProps) {
 		super(scope, id, props);
 
-		const vpc = GuVpc.fromIdParameter(this, "primary-vpc", {
+		const vpc = GuVpc.fromIdParameter(this, "primary-vpc", {});
+		const subnets = GuVpc.subnetsFromParameter(this, {
+			app: APP_NAME,
+			type: SubnetType.PRIVATE,
+		});
 
+		const cluster = new ElasticPlaygroundCluster(this, "playground", {
+			vpc: vpc,
+			subnets: subnets,
 		})
 	}
 }
